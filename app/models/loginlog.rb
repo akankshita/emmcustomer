a081e11eee9ebb5695e3127e20e556797c3ec9d7
tt-09 <tt-09@tt-09.(none)>
2013-01-01 17:01:08 +0530

    first commit

--- app/models/loginlog.rb
+++ app/models/loginlog.rb
@@ -1,3 +1,3 @@
 class Loginlog < ActiveRecord::Base
-   attr_accessible :name, :email,:login_time,:logout_time,:ip
+   attr_accessible :first_name,:last_name, :email,:login_time,:logout_time,:ip
 end
