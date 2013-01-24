af004f90fd504e37e98151467d06814fec06ce68
tt-09 <tt-09@tt-09.(none)>
2013-01-24 17:12:25 +0530

    cron test

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,23 +4,27 @@ class UserMailer < ActionMailer::Base
     #@user = user
     #attachments["rails.png"] = File.read("#{Rails.root}/public/images/rails.png")
     mail(:to => "akankshita.satapathy@php2india.com", :subject => "Testing")
+    mail(:to => "daniel@php2india.com", :subject => "Testing")
   end
   
   def atest()
     #@user = user
     #attachments["rails.png"] = File.read("#{Rails.root}/public/images/rails.png")
     mail(:to => "akankshita.satapathy@php2india.com", :subject => "Another Testing")
+    mail(:to => "daniel@php2india.com", :subject => "Another Testing")
   end
   
   def ipnotavaialable()
     #@user = user
     #attachments["rails.png"] = File.read("#{Rails.root}/public/images/rails.png")
     mail(:to => "akankshita.satapathy@php2india.com", :subject => "Ip Not Available")
+    mail(:to => "daniel@php2india.com", :subject => "Ip Not Available")
   end
   def incorrecttime()
     #@user = user
     #attachments["rails.png"] = File.read("#{Rails.root}/public/images/rails.png")
     mail(:to => "akankshita.satapathy@php2india.com", :subject => "Incorrect Time")
+    mail(:to => "daniel@php2india.com", :subject => "Incorrect Time")
   end
   
 end
