bb2e2125b729cfe9bc6338b02326e7af0a966d39
tt-09 <tt-09@tt-09.(none)>
2013-01-25 10:14:24 +0530

    dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
    before_filter :check_login
   def check_login
+=begin    
     ActiveRecord::Base.establish_connection(
         :adapter  => "postgresql",
         :host     => "ec2-54-243-238-144.compute-1.amazonaws.com",#"ec2-54-243-238-144.compute-1.amazonaws.com",
@@ -10,6 +11,7 @@ class ApplicationController < ActionController::Base
         :password =>"lC_HYsKxXsJerxoLpR_a5sMAwg", #"35JS51QKt5gQHm2HOH2D97p7kZ",
         :database => "d89hd8fvckog43"#"d5v3qoof2vr5rs"
     )
+=end    
    # render :text => $global_variable.inspect and return false
     if session[:login_id].nil?
        redirect_to '/'
